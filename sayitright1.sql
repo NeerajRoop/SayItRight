f9214a5214eb562c54e3396af41f8759df71e603
Neeraj Roop Venkatesan <62311050+NeerajRoop@users.noreply.github.com>
2020-12-09 18:41:02 -0600

    Add files via upload

---
+++ sayitright1.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 10, 2020 at 01:40 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sayitright1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `business`
+--
+-- Error reading structure for table sayitright1.business: #1932 - Table 'sayitright1.business' doesn't exist in engine
+-- Error reading data for table sayitright1.business: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sayitright1`.`business`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buy`
+--
+-- Error reading structure for table sayitright1.buy: #1932 - Table 'sayitright1.buy' doesn't exist in engine
+-- Error reading data for table sayitright1.buy: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sayitright1`.`buy`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+-- Error reading structure for table sayitright1.cart: #1932 - Table 'sayitright1.cart' doesn't exist in engine
+-- Error reading data for table sayitright1.cart: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sayitright1`.`cart`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `confery`
+--
+-- Error reading structure for table sayitright1.confery: #1932 - Table 'sayitright1.confery' doesn't exist in engine
+-- Error reading data for table sayitright1.confery: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sayitright1`.`confery`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `confery1`
+--
+-- Error reading structure for table sayitright1.confery1: #1932 - Table 'sayitright1.confery1' doesn't exist in engine
+-- Error reading data for table sayitright1.confery1: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sayitright1`.`confery1`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+-- Error reading structure for table sayitright1.contact: #1932 - Table 'sayitright1.contact' doesn't exist in engine
+-- Error reading data for table sayitright1.contact: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sayitright1`.`contact`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eventadd`
+--
+-- Error reading structure for table sayitright1.eventadd: #1932 - Table 'sayitright1.eventadd' doesn't exist in engine
+-- Error reading data for table sayitright1.eventadd: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sayitright1`.`eventadd`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eventadd1`
+--
+-- Error reading structure for table sayitright1.eventadd1: #1932 - Table 'sayitright1.eventadd1' doesn't exist in engine
+-- Error reading data for table sayitright1.eventadd1: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sayitright1`.`eventadd1`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+-- Error reading structure for table sayitright1.events: #1932 - Table 'sayitright1.events' doesn't exist in engine
+-- Error reading data for table sayitright1.events: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sayitright1`.`events`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `individual`
+--
+-- Error reading structure for table sayitright1.individual: #1932 - Table 'sayitright1.individual' doesn't exist in engine
+-- Error reading data for table sayitright1.individual: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sayitright1`.`individual`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logindetails`
+--
+-- Error reading structure for table sayitright1.logindetails: #1932 - Table 'sayitright1.logindetails' doesn't exist in engine
+-- Error reading data for table sayitright1.logindetails: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sayitright1`.`logindetails`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
